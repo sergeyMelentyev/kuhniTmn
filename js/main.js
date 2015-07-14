2b33f39d12ebb51dec026a1c503117bd2177c1b2
Sergey Melentyev <sergey.melentyev@gmail.com>
2015-07-14 22:27:37 +0300

    new version

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var comingSoon = $('.comingSoon');
 
 var myVivus = new Vivus('centerSvgDunamic', {
     type: 'delayed',
-    duration: 300},
+    duration: 450},
     function(){
     	animateTheRest();
     });
@@ -17,5 +17,5 @@ var animateTheRest = function(){
 setTimeout(function(){
 	myVivus.stop();
 	gradientBackGroundAddOn.transition({ opacity: 100 }, 2000, 'ease');	
-	comingSoon.transition({x: '0px', opacity: 100}, 750, 'ease');
-}, 6000);
+	comingSoon.transition({x: '0px', opacity: 80}, 750, 'ease');
+}, 6500);
