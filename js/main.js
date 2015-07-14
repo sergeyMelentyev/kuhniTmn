c05cce3eeed4d69bcb36a398aca914dff99e9b87
Sergey Melentyev <sergey.melentyev@gmail.com>
2015-07-14 18:43:22 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
  "use strict";
+var gradientBackGroundAddOn = $('.gradientBackGroundAddOn');
+var svgDunamicElements = $('.svgDunamicElements');
+var comingSoon = $('.comingSoon');
 
-$(document).ready(function () {
+gradientBackGroundAddOn.transition({ opacity: 100, delay: 4500 }, 2000, 'ease');	
+comingSoon.transition({x: '0px', opacity: 100, delay: 6000}, 750, 'ease');
 
-	new Vivus('centerSvgDunamic', {
-	    type: 'delayed',
-	    duration: 500
-	});
-});
+new Vivus('centerSvgDunamic', {
+    type: 'delayed',
+    duration: 300},
+    function(){
+    	animateTheRest();
+    });
+
+var animateTheRest = function(){
+	console.log('test');
+};
