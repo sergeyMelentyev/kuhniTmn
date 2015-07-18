408b63bbc8f01b08f4d08d2d14d4a4f3b2486449
Sergey Melentyev <sergey.melentyev@gmail.com>
2015-07-19 00:09:29 +0300

    new version

--- js/main.js
+++ js/main.js
@@ -1,24 +1,102 @@
  "use strict";
-var gradientBackGroundAddOn = $('.gradientBackGroundAddOn');
-var svgDunamicElements = $('.svgDunamicElements');
-var comingSoon = $('.comingSoon');
-var centerSvgDunamicFinal = $('#centerSvgDunamicFinal');
+var preLoaderView = $('.preLoaderView'),
+	pleaseWaitWrapper = $('.pleaseWaitWrapper'),
+	readyToLaunch = $('.readyToLaunch'),
+	svgBackGroundComplete = $('.svgBackGroundComplete'),
+	timeoutID,
+	fullpage = $('#fullpage'),
+	mainContantWrapper = $('.mainContantWrapper');
 
-var myVivus = new Vivus('centerSvgDunamic', {
+var myVivus = new Vivus('svgBackGroundDrawingId', {
     type: 'delayed',
-    duration: 250},
+    duration: 1000},
     function(){
-    	animateTheRest();
+    	console.log('test');
     });
 
-var animateTheRest = function(){
-	console.log('test');
-};
+window.addEventListener("load", function load(event){
+    window.removeEventListener("load", load, false); 
+    delayedStart();
+    console.log('Load Event Fires!'); 
+},false);
 
-setTimeout(function(){
+function delayedStart() {
+  timeoutID = window.setTimeout(startApplication, 1000);
+  console.log('Delayed Start Fires!'); 
+}
+
+function startApplication () {
+    console.log('Start Application Fires!');
 	myVivus.stop();
-	centerSvgDunamicFinal.css({'display': 'initial'});
-	centerSvgDunamicFinal.transition({ opacity: 100 }, 2000, 'ease');
-	gradientBackGroundAddOn.transition({ opacity: 100 }, 2000, 'ease');
-	comingSoon.transition({x: '0px', opacity: 80}, 750, 'ease');
-}, 5000);
+	svgBackGroundComplete.transition({ opacity: 100 }, 500);
+	readyToLaunch.transition({ opacity: 100 }, 500);
+	pleaseWaitWrapper.transition({ opacity: 0, delay: 500 }, 500);
+	setTimeout(function(){
+		pleaseWaitWrapper.css({'display': 'none'});
+	}, 1000);
+	readyToLaunch.click(function(){
+		preLoaderView.transition({ scale: 0.9 }, 250).transition({ y: -1000, opacity: 0 }, 500);
+		setTimeout(function(){
+			mainContantWrapper.css({'overflow': 'initial'});
+			mainContantWrapper.css({'height': 'auto'});
+		}, 600);
+		setTimeout(function(){
+			fullpage.fullpage({
+				menu: false,
+				lockAnchors: false,
+				anchors:['Sales', 'Quality', 'Portfolio', 'Goods', 'Contacts'],
+				navigation: false,
+				navigationPosition: 'right',
+				navigationTooltips: ['firstSlide', 'secondSlide'],
+				showActiveTooltip: false,
+				slidesNavigation: true,
+				slidesNavPosition: 'bottom',
+				css3: true,
+				scrollingSpeed: 700,
+				autoScrolling: true,
+				fitToSection: true,
+				scrollBar: false,
+				easing: 'easeInOutCubic',
+				easingcss3: 'ease',
+				loopBottom: false,
+				loopTop: false,
+				loopHorizontal: true,
+				continuousVertical: false,
+				normalScrollElements: '#element1, .element2',
+				scrollOverflow: false,
+				touchSensitivity: 15,
+				normalScrollElementTouchThreshold: 5,
+				keyboardScrolling: true,
+				animateAnchor: true,
+				recordHistory: true,
+				controlArrows: true,
+				verticalCentered: true,
+				resize : false,
+				sectionsColor : [''],
+				paddingTop: '',
+				paddingBottom: '',
+				fixedElements: '#header, .footer',
+				responsiveWidth: 0,
+				responsiveHeight: 0,
+				sectionSelector: '.section',
+				slideSelector: '.slide',
+				onLeave: function(index, nextIndex, direction){},
+				afterLoad: function(anchorLink, index){},
+				afterRender: function(){},
+				afterResize: function(){},
+				afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+				onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+			});	
+			}, 600);
+	});
+  };
+
+
+
+
+
+
+
+
+
+
