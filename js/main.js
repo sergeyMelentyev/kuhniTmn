4746fc3bcafd64eefdd588a0afe43c4cc1f6d441
Sergey Melentyev <sergey.melentyev@gmail.com>
2015-07-31 14:29:49 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
  "use strict";
+/* VARIABLES */
 var preLoaderView = $('.preLoaderView'),
 	pleaseWaitWrapper = $('.pleaseWaitWrapper'),
 	readyToLaunch = $('.readyToLaunch'),
 	svgBackGroundComplete = $('.svgBackGroundComplete'),
+	setGalleryViewCounter = 0,
 	timeoutID,
+	myVivus,
+	thirdPageVivus,
 	fullpage = $('#fullpage'),
 	mainContantWrapper = $('.mainContantWrapper'),
 	secondPageHeader = $('.secondPage header'),
@@ -17,26 +21,41 @@ var preLoaderView = $('.preLoaderView'),
 	thirdPageGalleryReadyIcon = $('.thirdPageGalleryReadyIcon'),
 	thirdPageGalleryScetchIcon = $('.thirdPageGalleryScetchIcon');
 
-var myVivus = new Vivus('svgBackGroundDrawingId', {
-    type: 'delayed',
-    duration: 1000},
-    function(){
-    	console.log('test');
-    });
 
+/* SVG ANIMATION  */
+var drawSvgMainTheme = function(){
+		myVivus = new Vivus('svgBackGroundDrawingId', {
+	    type: 'delayed',
+	    duration: 1000},
+	    function(){
+	    	console.log('MAIN SVG BG DONE');
+	    });
+};
+
+var drawSvgThirdPage = function(){
+		thirdPageVivus = new Vivus('backSvgWrapperDraw', {
+	    type: 'delayed',
+	    duration: 500},
+	    function(){
+	    	console.log('QUALITY PAGE SVG DONE');
+	    });	
+};
+
+/* MAIN APP STARTER */
 window.addEventListener("load", function load(event){
     window.removeEventListener("load", load, false); 
+    drawSvgMainTheme();
     delayedStart();
-    console.log('Load Event Fires!'); 
+    console.log('ONLOAD EVENT FIRES'); 
 },false);
 
 function delayedStart() {
   timeoutID = window.setTimeout(startApplication, 1000);
-  console.log('Delayed Start Fires!'); 
+  console.log('DELAYED START 1000MS DONE'); 
 }
 
 function startApplication () {
-    console.log('Start Application Fires!');
+    console.log('APP READY TO GO');
 	myVivus.stop();
 	svgBackGroundComplete.transition({ opacity: 100 }, 500);
 	readyToLaunch.transition({ opacity: 100 }, 500);
@@ -49,12 +68,13 @@ function startApplication () {
 		setTimeout(function(){
 			mainContantWrapper.css({'overflow': 'initial'});
 			mainContantWrapper.css({'height': 'auto'});
+			thirdPageGalleryWrapper.css({'overflow': 'initial'});
 		}, 1000);
 		setTimeout(function(){
 			fullpage.fullpage({
 				menu: false,
 				lockAnchors: false,
-				anchors:['Sales', 'Quality', 'Portfolio', 'Goods', 'Contacts'],
+				anchors:['Question', 'Sales', 'Quality', 'Portfolio', 'Goods', 'Contacts'],
 				navigation: false,
 				navigationPosition: '',
 				navigationTooltips: ['firstSlide', 'secondSlide'],
@@ -92,15 +112,38 @@ function startApplication () {
 				slideSelector: '.slide',
 				onLeave: function(index, nextIndex, direction){
 					console.log("onLeave--" + "index: " + index + " nextIndex: " + nextIndex + " direction: " +  direction);
-					if (index === 2 && nextIndex === 1) { // переход на первую страницу со второй
+					if (index === 1 && nextIndex === 2) {
 						$('.bgvid').get(0).play();
+					};
+					if (index === 3 && nextIndex === 2) {
+						$('.bgvid').get(0).play();
+					};
+					if (index === 4 && nextIndex === 3) {
+						if (setGalleryViewCounter === 1) {
+							animationEndFirstSlide();
+						};
+					};
+					if (index === 3 && nextIndex === 4) {
+						if (setGalleryViewCounter === 1) {
+							animationStartFirstSlide();
+						};
+					};
+					if (index === 4 && nextIndex === 5) {
+						if (setGalleryViewCounter === 1) {
+							animationEndFirstSlide();
+						};
+					};
+					if (index === 5 && nextIndex === 4) {
+						if (setGalleryViewCounter === 1) {
+							animationStartFirstSlide();
+						};
 					};
 				},
 				afterLoad: function(anchorLink, index){
 					if(anchorLink == 'Quality'){
 						secondPageHeader.transition({ y: -50, opacity: 80 }, 1000);
 						secondPageIconsWrapper.transition({ y: -50, opacity: 80 }, 1000);
-						console.log('Second page animation fires!');
+						console.log('THIRD PAGE ANIMATION FIRES');
 					};
 					if (anchorLink == 'Portfolio') {
 						leftSvgHeader.transition({ x: 130, opacity: 100 }, 1000);
@@ -108,79 +151,25 @@ function startApplication () {
 						if (backSvgWrapperDrawCounter === 0) {
 					    	backSvgWrapperDrawCounter++;
 							$('#backSvgWrapperDraw').transition({ opacity: 100 }, 500);
-							var thirdsPageVivus = new Vivus('backSvgWrapperDraw', {
-						    type: 'delayed',
-						    duration: 500},
-						    function(){
-						    	console.log('Ресуем на доске');
-						    });
+							drawSvgThirdPage();
 						};
 					};
 				},
-				afterRender: function(){
-					$('.bgvid').get(0).play();
-					console.log('Video fires after page rendered!')
-				},
+				afterRender: function(){},
 				afterResize: function(){},
 				afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){
 					if (slideAnchor == 'slide2') {
-						console.log('secondSlide fires!!!');
+						console.log('GALLERY SLIDE FIRES');
 					};
 				},
 				onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){
-					if (slideIndex == 0 && nextSlideIndex == 1 && direction == 'right') {
-						setTimeout(function(){
-							thirdPageGalleryWrapper.css({'visibility': 'initial'});
-							thirdPageGalleryScetchIcon.transition({ x: '200px', opacity: 0.2 }, 500);
-							if (thirdPageGalleryReadyIcon.hasClass('activeGallery')) {
-								thirdPageGalleryReadyIcon.transition({ x: '0px', opacity: 0.9 }, 500);
-								thirdPageGalleryReady.css({'visibility': 'initial'}).transition({ opacity: 1 }, 500);
-								thirdPageGalleryScetch.css({'visibility': 'hidden'}).transition({ opacity: 0 }, 500);
-							} else {
-								return false;
-							};
-							if (thirdPageGalleryScetchIcon.hasClass('activeGallery')) {
-								thirdPageGalleryScetchIcon.transition({ x: '0px', opacity: 0.9 }, 500);
-								thirdPageGalleryReady.css({'visibility': 'hidden'}).transition({ opacity: 0 }, 500);
-								thirdPageGalleryScetch.css({'visibility': 'initial'}).transition({ opacity: 1 }, 500);
-							} else {
-								return false;
-							};
-						}, 350);
-						$('.thirdPageGalleryReadyIcon img').click(function(){
-							console.log('ReadyIcon');
-							if (thirdPageGalleryReadyIcon.hasClass('activeGallery')) {
-								return false;
-							} else {
-								thirdPageGalleryReadyIcon.transition({ x: '0px', opacity: 0.9 }, 500);
-								thirdPageGalleryReadyIcon.addClass('activeGallery');
-								thirdPageGalleryScetchIcon.transition({ x: '200px', opacity: 0.2 }, 500);
-								thirdPageGalleryScetchIcon.removeClass('activeGallery');
-								thirdPageGalleryReady.css({'visibility': 'initial'}).transition({ opacity: 1 }, 500);
-								thirdPageGalleryReady.css({'display': 'initial'}).transition({ opacity: 1 }, 500);
-								thirdPageGalleryScetch.css({'display': 'none'}).transition({ opacity: 0 }, 500);
-							};
-						});
-						$('.thirdPageGalleryScetchIcon img').click(function(){
-							console.log('ScetchIcon');
-							if (thirdPageGalleryScetchIcon.hasClass('activeGallery')) {
-								return false;
-							} else {
-								thirdPageGalleryScetchIcon.transition({ x: '0px', opacity: 0.9 }, 500);
-								thirdPageGalleryScetchIcon.addClass('activeGallery');
-								thirdPageGalleryReadyIcon.transition({ x: '200px', opacity: 0.2 }, 500);
-								thirdPageGalleryReadyIcon.removeClass('activeGallery');
-								thirdPageGalleryScetch.css({'visibility': 'initial'}).transition({ opacity: 1 }, 500);
-								thirdPageGalleryScetch.css({'display': 'initial'}).transition({ opacity: 1 }, 500);
-								thirdPageGalleryReady.css({'display': 'none'}).transition({ opacity: 0 }, 500);
-							};
-						});
-						console.log('gallery is ready for view!');
+					if (slideIndex == 0 && nextSlideIndex == 1 && direction == 'right') { // Переход к первому слайду галереи
+						animationStartFirstSlide();
+						setGalleryViewCounter = 1;
 					};
-					if (slideIndex == 1 && nextSlideIndex == 0 && direction == 'left') {
-						thirdPageGalleryWrapper.css({'visibility': 'hidden'});
-						testFunctionEnd();
-						console.log('welcome back!!!');
+					if (slideIndex == 1 && nextSlideIndex == 0 && direction == 'left') { // возврат из галереи
+						animationEndFirstSlide();
+						setGalleryViewCounter = 0;
 					};
 					console.log("onLeave--" + "slideIndex: " + slideIndex + " nextSlideIndex: " + nextSlideIndex + " direction: " +  direction);
 				}
@@ -189,7 +178,8 @@ function startApplication () {
 	});
   };
 
-$('.slider-for').slick({
+/* INITIALIZATION */
+$('.slider-for').slick({ // первый слайдер галереи с готовыми кухняли
 	slidesToShow: 1,
 	slidesToScroll: 1,
 	arrows: false,
@@ -208,7 +198,7 @@ $('.slider-nav').slick({
 	focusOnSelect: true
 });
 
-$('.slider-for-scetch').slick({
+$('.slider-for-scetch').slick({ // первый слайдер галереи со скетчами
 	slidesToShow: 1,
 	slidesToScroll: 1,
 	arrows: false,
@@ -227,7 +217,32 @@ $('.slider-nav-scetch').slick({
 	focusOnSelect: true
 });
 
-var testFunctionEnd = function(){
+/* ANIMATION FUNCTIONS */
+var animationStartFirstSlide = function(){ // Переход к первому слайду галереи
+	setGalleryViewCounter = 1;
+	setTimeout(function(){
+		thirdPageGalleryWrapper.css({'visibility': 'initial'});
+		thirdPageGalleryScetchIcon.transition({ x: '200px', opacity: 0.2 }, 500);
+		if (thirdPageGalleryReadyIcon.hasClass('activeGallery')) {
+			thirdPageGalleryReadyIcon.transition({ x: '0px', opacity: 0.9 }, 500);
+			thirdPageGalleryReady.css({'visibility': 'initial'}).transition({ opacity: 1 }, 500);
+			thirdPageGalleryScetch.css({'visibility': 'hidden'}).transition({ opacity: 0 }, 500);
+		} else {
+			return false;
+		};
+		if (thirdPageGalleryScetchIcon.hasClass('activeGallery')) {
+			thirdPageGalleryScetchIcon.transition({ x: '0px', opacity: 0.9 }, 500);
+			thirdPageGalleryReady.css({'visibility': 'hidden'}).transition({ opacity: 0 }, 500);
+			thirdPageGalleryScetch.css({'visibility': 'initial'}).transition({ opacity: 1 }, 500);
+		} else {
+			return false;
+		};
+	}, 350);
+	console.log('GALLERY LAUNCHED');
+};
+
+var animationEndFirstSlide = function(){ // Выход из первого слайда галереи
+	thirdPageGalleryWrapper.css({'visibility': 'hidden'});
 	thirdPageGalleryReady.css({'display': 'initial'});
 	thirdPageGalleryScetch.css({'display': 'initial'});
 	thirdPageGalleryReady.css({'visibility': 'hidden'});
@@ -237,5 +252,33 @@ var testFunctionEnd = function(){
 		thirdPageGalleryScetchIcon.removeClass('activeGallery');
 		thirdPageGalleryReadyIcon.addClass('activeGallery');
 	};
+	console.log('GALLERY OFF');
 };
 
+/* ONCLICK EVENTS*/
+$('.thirdPageGalleryReadyIcon img').click(function(){ // Клик в галереи готовых работ по фото
+	if (thirdPageGalleryReadyIcon.hasClass('activeGallery')) {
+		return false;
+	} else {
+		thirdPageGalleryReadyIcon.transition({ x: '0px', opacity: 0.9 }, 500);
+		thirdPageGalleryReadyIcon.addClass('activeGallery');
+		thirdPageGalleryScetchIcon.transition({ x: '200px', opacity: 0.2 }, 500);
+		thirdPageGalleryScetchIcon.removeClass('activeGallery');
+		thirdPageGalleryReady.css({'visibility': 'initial'}).transition({ opacity: 1 }, 500);
+		thirdPageGalleryReady.css({'display': 'initial'}).transition({ opacity: 1 }, 500);
+		thirdPageGalleryScetch.css({'display': 'none'}).transition({ opacity: 0 }, 500);
+	};
+});
+$('.thirdPageGalleryScetchIcon img').click(function(){ // Клик в галереи готовых работ по скетчам
+	if (thirdPageGalleryScetchIcon.hasClass('activeGallery')) {
+		return false;
+	} else {
+		thirdPageGalleryScetchIcon.transition({ x: '0px', opacity: 0.9 }, 500);
+		thirdPageGalleryScetchIcon.addClass('activeGallery');
+		thirdPageGalleryReadyIcon.transition({ x: '200px', opacity: 0.2 }, 500);
+		thirdPageGalleryReadyIcon.removeClass('activeGallery');
+		thirdPageGalleryScetch.css({'visibility': 'initial'}).transition({ opacity: 1 }, 500);
+		thirdPageGalleryScetch.css({'display': 'initial'}).transition({ opacity: 1 }, 500);
+		thirdPageGalleryReady.css({'display': 'none'}).transition({ opacity: 0 }, 500);
+	};
+});
